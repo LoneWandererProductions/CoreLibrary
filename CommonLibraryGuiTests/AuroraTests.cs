09835392db1827da8c697f68b7bf2a85aa32ebe4
Wayfarer <petergeinitz@freenet.de>
2024-01-08 10:54:24 +0100

    prepare some tests

---
+++ CommonLibraryGuiTests/AuroraTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace CommonLibraryGuiTests
+{
+    public sealed class AuroraTests
+    {
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void ExtendedGrids()
+        {
+        }
+    }
+}
