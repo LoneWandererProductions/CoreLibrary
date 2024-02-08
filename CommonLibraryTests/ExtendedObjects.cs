cc3d06f305f955bcf9595b5eee49bf7c450a5633
Wayfarer <petergeinitz@freenet.de>
2024-02-08 17:55:44 +0100

    Had some fun with a custom dictionary. Not as fast or clean as Microsofts but a good template

---
+++ CommonLibraryTests/ExtendedObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using ExtendedSystemObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonLibraryTests
+{
+    [TestClass]
+    public class ExtendedObjects
+    {
+        [TestMethod]
+        public void ExtendedDictionary()
+        {
+            ExDictionary<int, int> dict = new ExDictionary<int, int>();
+            dict.Add(-1, -1);
+            dict.Add(1, -1);
+            dict.Add(9, -1);
+            dict.ContainsKey(9);
+            dict.GetValue(9);
+            dict[10] = 10;
+
+            foreach (var item in dict)
+            {
+                Trace.WriteLine(item.Key);
+                Trace.WriteLine(item.Value);
+            }
+            Trace.WriteLine(dict[10]);
+        }
+    }
+}
