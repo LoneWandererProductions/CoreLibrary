fb8e9477692d8e4b727f3d15eb528477524869a1
Wayfarer <petergeinitz@freenet.de>
2024-02-17 11:45:24 +0100

    Documentation

--- Mathematics/Transform.cs
+++ Mathematics/Transform.cs
@@ -1,4 +1,12 @@
-﻿namespace Mathematics
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     Mathematics
+ * FILE:        Mathematics/Transform.cs
+ * PURPOSE:     Controller Class for all 3D Projections and Camera
+ * PROGRAMER:   Peter Geinitz (Wayfarer)
+ */
+
+namespace Mathematics
 {
     /// <summary>
     /// Basic Controller for the World and the camera
@@ -35,7 +43,7 @@
         /// <value>
         /// The target.
         /// </value>
-        public Vector3D Target { get; set; } = new Vector3D(0,0,1);
+        public Vector3D Target { get; set; } = new Vector3D(0, 0, 1);
 
         /// <summary>
         /// Gets or sets the look Vector direction.
@@ -69,52 +77,92 @@
         /// </value>
         public Vector3D Scale { get; set; } = Vector3D.UnitVector;
 
+        /// <summary>
+        /// Moves the world.
+        /// </summary>
+        /// <param name="translation">The translation.</param>
         public void MoveWorld(Vector3D translation)
         {
             Translation += translation;
         }
 
+        /// <summary>
+        /// Rotates the world.
+        /// </summary>
+        /// <param name="rotation">The rotation.</param>
         public void RotateWorld(Vector3D rotation)
         {
             Rotation += rotation;
         }
 
+        /// <summary>
+        /// Rotates the camera left.
+        /// </summary>
+        /// <param name="angle">The angle.</param>
         public void RotateCameraLeft(double angle)
         {
             Angle += angle;
         }
 
+        /// <summary>
+        /// Rotates the camera right.
+        /// </summary>
+        /// <param name="angle">The angle.</param>
         public void RotateCameraRight(double angle)
         {
             Angle -= angle;
         }
 
+        /// <summary>
+        /// Ups the camera.
+        /// </summary>
+        /// <param name="y">The y.</param>
         public void UpCamera(double y)
         {
             Camera.Y += y;
         }
 
+        /// <summary>
+        /// Downs the camera.
+        /// </summary>
+        /// <param name="y">The y.</param>
         public void DownCamera(double y)
         {
             Camera.Y -= y;
         }
 
+        /// <summary>
+        /// Lefts the camera.
+        /// </summary>
+        /// <param name="x">The x.</param>
         public void LeftCamera(double x)
         {
             Camera.X -= x;
         }
 
+        /// <summary>
+        /// Rights the camera.
+        /// </summary>
+        /// <param name="x">The x.</param>
         public void RightCamera(double x)
         {
             Camera.X -= x;
         }
 
+        /// <summary>
+        /// Lefts the rotate camera.
+        /// </summary>
+        /// <param name="value">The value.</param>
         public void LeftRotateCamera(double value)
         {
             var vForward = VLookDir * value;
             Camera += vForward;
         }
 
+        /// <summary>
+        /// Rights the rotate camera.
+        /// </summary>
+        /// <param name="value">The value.</param>
         public void RightRotateCamera(double value)
         {
             var vForward = VLookDir * value;
