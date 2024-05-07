51c5af399252aac35f9dcab2f7597d24619939be
LoneWandererProductions <petergeinitz@freenet.de>
2024-05-07 15:59:56 +0200

    add some network stuff

---
+++ Communication/NetworkFolder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Communication
+{
+    internal class NetworkFolder
+    {
+        public void SearchNetworkFolder(string path)
+        {
+            // Create a new FileSystemWatcher instance
+            FileSystemWatcher watcher = new FileSystemWatcher(path)
+            {
+                // Set the FileSystemWatcher properties
+                NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName,
+                IncludeSubdirectories = true
+            };
+
+            // Subscribe to the Deleted event
+            watcher.Deleted += OnDeleted;
+
+            // Start monitoring
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Monitoring folder for deletions. Press any key to exit.");
+            Console.ReadKey();
+
+            // Stop monitoring
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            // Handle the deletion event
+            Console.WriteLine($"File or folder '{e.FullPath}' was deleted.");
+        }
+
+    }
+}
