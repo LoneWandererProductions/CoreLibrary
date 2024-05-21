906d75cb6c6fbc8dbc4f6c77400e98063c141eb8
LoneWandererProductions <petergeinitz@freenet.de>
2024-05-21 13:52:08 +0200

    improve image slider

--- ImageCompare/ImageSlider.cs
+++ ImageCompare/ImageSlider.cs
@@ -5,6 +5,14 @@ namespace ImageCompare
 {
     internal static class ImageSlider
     {
+        /// <summary>
+        /// Determines whether the small image is part of the big image.
+        /// </summary>
+        /// <param name="bigImage">The big image.</param>
+        /// <param name="smallImage">The small image.</param>
+        /// <returns>
+        ///   <c>true</c> if the small image is part of the big image; otherwise, <c>false</c>.
+        /// </returns>
         internal static bool IsPartOf(Bitmap bigImage, Bitmap smallImage)
         {
             int bigHeight = bigImage.Height;
@@ -12,30 +20,42 @@ namespace ImageCompare
             int smallHeight = smallImage.Height;
             int smallWidth = smallImage.Width;
 
-            var dbmBig = new DirectBitmap(bigImage);
-            var dbmSmall = new DirectBitmap(smallImage);
+            using var dbmBig = new DirectBitmap(bigImage);
+            using var dbmSmall = new DirectBitmap(smallImage);
 
             var smallImageBottomEdge = new DirectBitmap(smallWidth, 1);
-
-            for (int x = 0; x < smallWidth; x++)
-            {
-                smallImageBottomEdge.SetPixel(x, 0, dbmSmall.GetPixel(x, smallHeight - 1));
-            }
 
             for (int i = 0; i <= bigHeight - smallHeight; i++)
             {
                 for (int j = 0; j <= bigWidth - smallWidth; j++)
                 {
+                    // Update the bottom edge for the current position in the big image
+                    for (int x = 0; x < smallWidth; x++)
+                    {
+                        smallImageBottomEdge.SetPixel(x, 0, dbmBig.GetPixel(j + x, i + smallHeight - 1));
+                    }
+
                     if (CheckEdges(dbmBig, dbmSmall, i, j, smallImageBottomEdge) && CheckFull(dbmBig, dbmSmall, i, j))
                     {
+                        smallImageBottomEdge.Dispose();
                         return true;
                     }
                 }
             }
 
+            smallImageBottomEdge.Dispose();
             return false;
         }
 
+        /// <summary>
+        /// Checks the edges of the small image within the big image.
+        /// </summary>
+        /// <param name="bigImage">The big image.</param>
+        /// <param name="smallImage">The small image.</param>
+        /// <param name="startRow">The start row.</param>
+        /// <param name="startCol">The start col.</param>
+        /// <param name="smallImageBottomEdge">The small image bottom edge.</param>
+        /// <returns>Basic check of the edges</returns>
         private static bool CheckEdges(DirectBitmap bigImage, DirectBitmap smallImage, int startRow, int startCol, DirectBitmap smallImageBottomEdge)
         {
             int smallHeight = smallImage.Height;
@@ -80,6 +100,16 @@ namespace ImageCompare
             return true;
         }
 
+        /// <summary>
+        /// Fully checks if the images match in the defined area.
+        /// </summary>
+        /// <param name="bigImage">The big image.</param>
+        /// <param name="smallImage">The small image.</param>
+        /// <param name="startRow">The start row.</param>
+        /// <param name="startCol">The start col.</param>
+        /// <returns>
+        /// True if images are equal in the defined area, otherwise false.
+        /// </returns>
         private static bool CheckFull(DirectBitmap bigImage, DirectBitmap smallImage, int startRow, int startCol)
         {
             int smallHeight = smallImage.Height;
