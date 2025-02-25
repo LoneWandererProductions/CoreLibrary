42e9fabaabc960461d66b1825b3be3af84a0f647
LoneWandererProductions <petergeinitz@freenet.de>
2025-02-25 08:58:49 +0100

    Add some simplified math Stuff.

---
+++ CommonLibraryTests/TrigBenchmarkTests.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     TrigBenchmarkTests
+ * FILE:        TrigBenchmarkTests/TrigBenchmarkTests.cs
+ * PURPOSE:     Test some speed options
+ * PROGRAMER:   Peter Geinitz (Wayfarer)
+ */
+
+using System;
+using System.Diagnostics;
+using Mathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonLibraryTests
+{
+    /// <summary>
+    /// Tests for some simplifications
+    /// </summary>
+    [TestClass]
+    public class TrigBenchmarkTests
+    {
+        /// <summary>
+        /// The iterations
+        /// </summary>
+        private const int iterations = 1_000_000;
+
+        /// <summary>
+        /// The x
+        /// </summary>
+        private const float x = 1.2345f;
+
+        /// <summary>
+        /// Compares the sin functions.
+        /// </summary>
+        [TestMethod]
+        public void CompareSinFunctions()
+        {
+            Compare(nameof(FastMath.FastSin), FastMath.FastSin, MathF.Sin);
+            Compare(nameof(FastMath.MediumSin), FastMath.MediumSin, MathF.Sin);
+            Compare(nameof(FastMath.LUTSin), FastMath.LUTSin, MathF.Sin);
+        }
+
+        /// <summary>
+        /// Compares the cos functions.
+        /// </summary>
+        [TestMethod]
+        public void CompareCosFunctions()
+        {
+            Compare(nameof(FastMath.FastCos), FastMath.FastCos, MathF.Cos);
+        }
+
+        /// <summary>
+        /// Compares the specified method name.
+        /// </summary>
+        /// <param name="methodName">Name of the method.</param>
+        /// <param name="fastFunc">The fast function.</param>
+        /// <param name="refFunc">The reference function.</param>
+        private static void Compare(string methodName, Func<float, float> fastFunc, Func<float, float> refFunc)
+        {
+            var fastResult = 0f;
+            var refResult = 0f;
+            var errorSum = 0f;
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                fastResult = fastFunc(x);
+                refResult = refFunc(x);
+                errorSum += MathF.Abs(0 - refResult);
+            }
+
+            sw.Stop();
+            Trace.WriteLine($"{methodName}: {sw.ElapsedMilliseconds} ms, avg error={errorSum / iterations}");
+        }
+    }
+}
---
+++ Mathematics/FastMath.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     Mathematics
+ * FILE:        Mathematics/FastMath.cs
+ * PURPOSE:     Simple optimiced Math Functions
+ * PROGRAMER:   Peter Geinitz (Wayfarer)
+ */
+
+using System;
+
+namespace Mathematics
+{
+    public static class FastMath
+    {
+        private const float Pi = (float)Math.PI;
+        private const float TwoPi = 2 * Pi;
+
+        /// <summary>
+        /// Fast approximation of sin(x) using a cubic polynomial.
+        /// </summary>
+        public static float FastSin(float x)
+        {
+            x = x % TwoPi; // Keep x in valid range
+            return x * (1.27323954f - 0.405284735f * Math.Abs(x));
+        }
+
+        /// <summary>
+        /// Fast approximation of cos(x) using the squared sin approximation.
+        /// </summary>
+        public static float FastCos(float x)
+        {
+            return FastSin(x + Pi / 2);
+        }
+
+        /// <summary>
+        /// Medium-accuracy sine approximation using a 5th-degree polynomial.
+        /// </summary>
+        public static float MediumSin(float x)
+        {
+            x = x % TwoPi;
+            float x2 = x * x;
+            return x * (1 - x2 / 6 + x2 * x2 / 120);
+        }
+
+        /// <summary>
+        /// Fast lookup table based sin(x)
+        /// </summary>
+        private static readonly float[] SinLUT = new float[256];
+        static FastMath()
+        {
+            for (int i = 0; i < SinLUT.Length; i++)
+                SinLUT[i] = (float)Math.Sin(i * TwoPi / SinLUT.Length);
+        }
+
+        public static float LUTSin(float x)
+        {
+            int index = (int)(x * (SinLUT.Length / TwoPi)) & (SinLUT.Length - 1);
+            return SinLUT[index];
+        }
+    }
+}
