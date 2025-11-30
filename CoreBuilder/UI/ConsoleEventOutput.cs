ad9929a026d28f94d08eeabe75fa48dceec2cecb
Wayfarer <peter.geinitz@freenet.de>
2025-11-30 09:37:47 +0100

    add a wpf window

--- CoreBuilder/UI/ConsoleEventOutput.cs
+++ CoreBuilder/UI/ConsoleEventOutput.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * COPYRIGHT:   See COPYING in the top level directory
- * PROJECT:     CoreBuilder.Helper
+ * PROJECT:     CoreBuilder.UI
  * FILE:        ConsoleEventOutput.cs
  * PURPOSE:     Sample console side channel for event output.
  * PROGRAMMER:  Peter Geinitz (Wayfarer)
@@ -9,17 +9,16 @@
 using System;
 using CoreBuilder.Interface;
 
-namespace CoreBuilder.Helper
+namespace CoreBuilder.UI
 {
     /// <inheritdoc />
     /// <summary>
     /// Console side channel for event output.
     /// </summary>
-    /// <seealso cref="CoreBuilder.Interface.IEventOutput" />
+    /// <seealso cref="IEventOutput" />
     public sealed class ConsoleEventOutput : IEventOutput
     {
         /// <inheritdoc />
         public void Write(string message) => Console.WriteLine(message);
     }
-
 }
