b2a428639a5a043abf92324595c1300e58a12eb5
LoneWandererProductions <peter.geinitz@freenet.de>
2026-03-17 11:43:17 +0100

    wire UDP into the facade

---
+++ Communication/LogProtocol.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     Communication
+ * FILE:        LogProtocol.cs
+ * PURPOSE:     List of Supported Log Protocols.
+ * PROGRAMMER:  Peter Geinitz (Wayfarer)
+ */
+
+namespace Communication
+{
+    /// <summary>
+    /// Enum of supported Protocols
+    /// </summary>
+    public enum LogProtocol
+    { 
+        TCP, 
+        UDP, 
+        Both 
+    }
+}
