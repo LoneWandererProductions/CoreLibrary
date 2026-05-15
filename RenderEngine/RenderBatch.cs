2351fc4fd6a48ebaee716cdd30e236b684f4e483
LoneWandererProductions <peter.geinitz@freenet.de>
2026-05-15 11:27:46 +0200

    Sync back some Render Engine Stuff and cleanup

--- RenderEngine/RenderBatch.cs
+++ RenderEngine/RenderBatch.cs
@@ -2,36 +2,92 @@
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     RenderEngine
  * FILE:        RenderBatch.cs
- * PURPOSE:     Your file purpose here
+ * PURPOSE:     Batches 2D and 3D draw calls to minimize state changes and draw calls.
  * PROGRAMMER:  Peter Geinitz (Wayfarer)
  */
 
-using ExtendedSystemObjects;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
+using ExtendedSystemObjects;
 
 namespace RenderEngine
 {
+    /// <summary>
+    /// Batches 2D and 3D draw calls to minimize state changes and draw calls.
+    /// </summary>
     public sealed class RenderBatch
     {
+        // --- 2D Storage ---
+
+        /// <summary>
+        /// The solid line vertices
+        /// </summary>
         public readonly UnmanagedList<float> SolidLineVertices = new(8192);
+
+        /// <summary>
+        /// The solid triangle vertices
+        /// </summary>
         public readonly UnmanagedList<float> SolidTriangleVertices = new(8192);
+
+        /// <summary>
+        /// The textured batches
+        /// </summary>
         public readonly Dictionary<int, List<float>> TexturedBatches = new();
 
+        // --- 3D Storage (Ported Tech) ---
+        /// <summary>
+        /// The solid 3D vertices
+        ///  Layout: X, Y, Z, R, G, B, A (7 floats)
+        /// </summary>
+        public readonly UnmanagedList<float> Solid3DVertices = new(16384);
+
+        // 
+        /// <summary>
+        /// The textured 3D batches
+        /// Layout: X, Y, Z, U, V, R, G, B, A (9 floats) - Grouped by TextureId
+        /// </summary>
+        public readonly Dictionary<int, List<float>> Textured3DBatches = new();
+
+        /// <summary>
+        /// The host actions
+        /// </summary>
         public readonly List<Action> HostActions = new();
 
+        /// <summary>
+        /// Adds the colored line.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="r">The r.</param>
+        /// <param name="g">The g.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="a">a.</param>
         public void AddColoredLine(float x, float y, int r, int g, int b, int a)
         {
-            // Pushes a single vertex into the line batch.
-            // When flushed, OpenGL will connect every 2 consecutive vertices into a line segment.
             AddColoredVertex(x, y, r, g, b, a, SolidLineVertices);
         }
 
+        /// <summary>
+        /// Adds the solid triangle vertex.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="r">The r.</param>
+        /// <param name="g">The g.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="a">a.</param>
         public void AddSolidTriangleVertex(float x, float y, int r, int g, int b, int a)
         {
             AddColoredVertex(x, y, r, g, b, a, SolidTriangleVertices);
         }
 
+        /// <summary>
+        /// Adds the colored triangle.
+        /// </summary>
+        /// <param name="v1">The v1.</param>
+        /// <param name="v2">The v2.</param>
+        /// <param name="v3">The v3.</param>
         public void AddColoredTriangle(
             (float x, float y, int r, int g, int b, int a) v1,
             (float x, float y, int r, int g, int b, int a) v2,
@@ -42,6 +98,14 @@ namespace RenderEngine
             AddColoredVertex(v3.x, v3.y, v3.r, v3.g, v3.b, v3.a, SolidTriangleVertices);
         }
 
+        /// <summary>
+        /// Adds the solid quad.
+        /// </summary>
+        /// <param name="p1">The p1.</param>
+        /// <param name="p2">The p2.</param>
+        /// <param name="p3">The p3.</param>
+        /// <param name="p4">The p4.</param>
+        /// <param name="color">The color.</param>
         public void AddSolidQuad(
             (int x, int y) p1, (int x, int y) p2,
             (int x, int y) p3, (int x, int y) p4,
@@ -56,33 +120,122 @@ namespace RenderEngine
             AddColoredTriangle(v1, v3, v4);
         }
 
-        public void AddTexturedQuad(
-            (int x, int y) p1, (int x, int y) p2,
-            (int x, int y) p3, (int x, int y) p4,
-            int textureId)
+        /// <summary>
+        /// Adds the textured quad.
+        /// </summary>
+        /// <param name="p1">The p1.</param>
+        /// <param name="p2">The p2.</param>
+        /// <param name="p3">The p3.</param>
+        /// <param name="p4">The p4.</param>
+        /// <param name="textureId">The texture identifier.</param>
+        public void AddTexturedQuad((int x, int y) p1, (int x, int y) p2, (int x, int y) p3, (int x, int y) p4, int textureId)
         {
             AddTexturedTriangle(p1, p2, p3, textureId);
             AddTexturedTriangle(p1, p3, p4, textureId);
         }
 
-        public void AddTexturedTriangle(
-            (int x, int y) p1, (int x, int y) p2, (int x, int y) p3,
-            int textureId)
+        /// <summary>
+        /// Adds the textured triangle.
+        /// </summary>
+        /// <param name="p1">The p1.</param>
+        /// <param name="p2">The p2.</param>
+        /// <param name="p3">The p3.</param>
+        /// <param name="textureId">The texture identifier.</param>
+        public void AddTexturedTriangle((int x, int y) p1, (int x, int y) p2, (int x, int y) p3, int textureId)
         {
-            // Grab the list for this specific texture, or create it if it doesn't exist
             if (!TexturedBatches.TryGetValue(textureId, out var list))
             {
-                // Pre-allocate the new list!
                 list = new List<float>(4096);
                 TexturedBatches[textureId] = list;
             }
 
-            // Notice we only push the 4 required floats, but into the correct texture's list
             AddTexturedVertex(p1.x, p1.y, 0f, 0f, list);
             AddTexturedVertex(p2.x, p2.y, 1f, 0f, list);
             AddTexturedVertex(p3.x, p3.y, 0f, 1f, list);
         }
 
+        /// <summary>
+        /// Adds a solid colored triangle to the 3D batch.
+        /// </summary>
+        /// <param name="v1">The v1.</param>
+        /// <param name="v2">The v2.</param>
+        /// <param name="v3">The v3.</param>
+        /// <param name="color">The color.</param>
+        public void AddSolid3DTriangle(Vector3 v1, Vector3 v2, Vector3 v3, (int r, int g, int b, int a) color)
+        {
+            AddColoredVertex3D(v1, color, Solid3DVertices);
+            AddColoredVertex3D(v2, color, Solid3DVertices);
+            AddColoredVertex3D(v3, color, Solid3DVertices);
+        }
+
+        /// <summary>
+        /// Adds a textured triangle to the 3D batch, grouped by texture ID.
+        /// </summary>
+        /// <param name="v1">The v1.</param>
+        /// <param name="uv1">The uv1.</param>
+        /// <param name="v2">The v2.</param>
+        /// <param name="uv2">The uv2.</param>
+        /// <param name="v3">The v3.</param>
+        /// <param name="uv3">The uv3.</param>
+        /// <param name="textureId">The texture identifier.</param>
+        public void AddTextured3DTriangle(Vector3 v1, Vector2 uv1, Vector3 v2, Vector2 uv2, Vector3 v3, Vector2 uv3, int textureId)
+        {
+            if (!Textured3DBatches.TryGetValue(textureId, out var list))
+            {
+                list = new List<float>(4096);
+                Textured3DBatches[textureId] = list;
+            }
+
+            AddTexturedVertex3D(v1, uv1, list);
+            AddTexturedVertex3D(v2, uv2, list);
+            AddTexturedVertex3D(v3, uv3, list);
+        }
+
+        /// <summary>
+        /// Helper for 3D textured vertex: X, Y, Z, U, V, R, G, B, A (9 floats).
+        /// Defaulting color to White (1,1,1,1).
+        /// </summary>
+        /// <param name="pos">The position.</param>
+        /// <param name="uv">The uv.</param>
+        /// <param name="targetList">The target list.</param>
+        private void AddTexturedVertex3D(Vector3 pos, Vector2 uv, List<float> targetList)
+        {
+            targetList.Add(pos.X);
+            targetList.Add(pos.Y);
+            targetList.Add(pos.Z);
+            targetList.Add(uv.X);
+            targetList.Add(uv.Y);
+            targetList.Add(1f); // R
+            targetList.Add(1f); // G
+            targetList.Add(1f); // B
+            targetList.Add(1f); // A
+        }
+
+        /// <summary>
+        /// Helper for 3D colored vertex: X, Y, Z, R, G, B, A (7 floats).
+        /// </summary>
+        /// <param name="pos">The position.</param>
+        /// <param name="c">The c.</param>
+        /// <param name="targetList">The target list.</param>
+        private void AddColoredVertex3D(Vector3 pos, (int r, int g, int b, int a) c, UnmanagedList<float> targetList)
+        {
+            targetList.Add(pos.X);
+            targetList.Add(pos.Y);
+            targetList.Add(pos.Z);
+            targetList.Add(c.r / 255f);
+            targetList.Add(c.g / 255f);
+            targetList.Add(c.b / 255f);
+            targetList.Add(c.a / 255f);
+        }
+
+        /// <summary>
+        /// Adds the textured vertex.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="u">The u.</param>
+        /// <param name="v">The v.</param>
+        /// <param name="targetList">The target list.</param>
         private void AddTexturedVertex(float x, float y, float u, float v, List<float> targetList)
         {
             targetList.Add(x);
@@ -91,8 +244,17 @@ namespace RenderEngine
             targetList.Add(v);
         }
 
-        private void AddColoredVertex(
-            float x, float y, int r, int g, int b, int a, UnmanagedList<float> targetList)
+        /// <summary>
+        /// Adds the colored vertex.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="r">The r.</param>
+        /// <param name="g">The g.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="a">a.</param>
+        /// <param name="targetList">The target list.</param>
+        private void AddColoredVertex(float x, float y, int r, int g, int b, int a, UnmanagedList<float> targetList)
         {
             targetList.Add(x);
             targetList.Add(y);
@@ -102,25 +264,36 @@ namespace RenderEngine
             targetList.Add(a / 255f);
         }
 
+        /// <summary>
+        /// Clears this instance.
+        /// </summary>
         public void Clear()
         {
             SolidLineVertices.Clear();
             SolidTriangleVertices.Clear();
+            Solid3DVertices.Clear();
 
-            // FIX: Clear the inner lists instead of destroying the dictionary items
             foreach (var list in TexturedBatches.Values)
             {
                 list.Clear();
             }
-            // Note: We leave the dictionary keys intact for the next frame!
+
+            foreach (var list in Textured3DBatches.Values)
+            {
+                list.Clear();
+            }
 
             HostActions.Clear();
         }
 
+        /// <summary>
+        /// Adds the host action.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <exception cref="System.ArgumentNullException">action</exception>
         public void AddHostAction(Action action)
         {
             if (action == null) throw new ArgumentNullException(nameof(action));
-
             HostActions.Add(action);
         }
     }
--- RenderEngine/Simple3DRenderer.cs
+++ RenderEngine/Simple3DRenderer.cs
@@ -2,15 +2,14 @@
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     RenderEngine
  * FILE:        Simple3DRenderer.cs
- * PURPOSE:     Lightweight 3D renderer using OpenGL
+ * PURPOSE:     Your file purpose here
  * PROGRAMMER:  Peter Geinitz (Wayfarer)
- * NOTES:       - Uses basic MVP (Model-View-Projection) matrices
- *              - Designed to work seamlessly alongside Simple2DRenderer
  */
 
 using OpenTK.Graphics.OpenGL4;
-using OpenTK.Mathematics;
 using System;
+using System.Numerics; 
+using TK = OpenTK.Mathematics;
 
 namespace RenderEngine
 {
@@ -18,147 +17,157 @@ namespace RenderEngine
     {
         public int Width { get; private set; }
         public int Height { get; private set; }
-
         private readonly GlResourceManager _resources;
 
-        private int _vao;
-        private int _vbo;
-        private int _shaderProgram;
+        private int _vaoSolid, _vboSolid, _vaoTex, _vboTex;
+        private int _shaderSolid, _shaderTex;
         private bool _initialized;
+        private int _vboSolidCapacity = 16384, _vboTexCapacity = 16384;
 
-        // Matrices
-        private Matrix4 _projection;
-        private Matrix4 _view;
+        private TK.Matrix4 _projection;
+        private TK.Matrix4 _view;
+
+        public TK.Matrix4 ViewMatrix => _view;
 
         public Simple3DRenderer(int width, int height, GlResourceManager resources)
         {
-            Width = width;
-            Height = height;
             _resources = resources;
-
-            // Default camera looking at the origin from slightly back and up
             UpdateProjection(width, height);
-            SetCamera(new Vector3(0, 2, 5), Vector3.Zero, Vector3.UnitY);
+            SetCamera(new Vector3(8, 15, 25), new Vector3(8, 0, 8), Vector3.UnitY);
         }
 
         public void UpdateProjection(int width, int height)
         {
-            // Prevent zero dimensions during WPF initialization or when minimized
             if (width <= 0) width = 1;
             if (height <= 0) height = 1;
-
-            Width = width;
-            Height = height;
-
+            Width = width; Height = height;
             var aspect = width / (float)height;
-
-            // 45 degree Field of View, Near plane 0.1f, Far plane 1000f
-            _projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), aspect, 0.1f, 1000f);
+            _projection = TK.Matrix4.CreatePerspectiveFieldOfView(TK.MathHelper.DegreesToRadians(45f), aspect, 0.1f, 1000f);
         }
 
         public void SetCamera(Vector3 position, Vector3 target, Vector3 up)
         {
-            _view = Matrix4.LookAt(position, target, up);
+            // Convert System.Numerics to OpenTK here using the extension or manual copy
+            _view = TK.Matrix4.LookAt(
+                new TK.Vector3(position.X, position.Y, position.Z),
+                new TK.Vector3(target.X, target.Y, target.Z),
+                new TK.Vector3(up.X, up.Y, up.Z));
         }
 
         private void EnsureInitialized()
         {
             if (_initialized) return;
-
-            // 1. Generate Buffers
-            _vao = GL.GenVertexArray();
-            _vbo = GL.GenBuffer();
-
-            GL.BindVertexArray(_vao);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
-
-            // Give it a decent starting capacity
-            GL.BufferData(BufferTarget.ArrayBuffer, 8192 * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw);
-
-            // 2. Vertex Attributes (Layout: X, Y, Z, R, G, B, A) -> 7 floats total
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 7 * sizeof(float), 0); // Position
+            _vaoSolid = GL.GenVertexArray(); _vboSolid = GL.GenBuffer();
+            GL.BindVertexArray(_vaoSolid); GL.BindBuffer(BufferTarget.ArrayBuffer, _vboSolid);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vboSolidCapacity * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 7 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 7 * sizeof(float),
-                3 * sizeof(float)); // Color
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 7 * sizeof(float), 3 * sizeof(float));
             GL.EnableVertexAttribArray(1);
 
+            _vaoTex = GL.GenVertexArray(); _vboTex = GL.GenBuffer();
+            GL.BindVertexArray(_vaoTex); GL.BindBuffer(BufferTarget.ArrayBuffer, _vboTex);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vboTexCapacity * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 9 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 9 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(2, 4, VertexAttribPointerType.Float, false, 9 * sizeof(float), 5 * sizeof(float));
+            GL.EnableVertexAttribArray(2);
             GL.BindVertexArray(0);
 
-            // 3. Fetch Shader from ResourceManager instead of compiling inline
-            _shaderProgram = _resources.GetShaderProgram(ShaderTypeApp.VertexColor);
-
+            _shaderSolid = _resources.GetShaderProgram(ShaderTypeApp.VertexColor);
+            _shaderTex = _resources.GetShaderProgram(ShaderTypeApp.TexturedQuad);
             _initialized = true;
         }
 
-        /// <summary>
-        /// Draws a colored cube at a specific 3D position with a given scale.
-        /// </summary>
-        public void DrawCube(Vector3 position, Vector3 scale, (int r, int g, int b, int a) color)
+        // --- FIXED SIGNATURES ---
+        public void DrawTriangle(Vector3 v0, Vector3 v1, Vector3 v2, (int r, int g, int b, int a) color)
         {
             EnsureInitialized();
-
-            GL.UseProgram(_shaderProgram);
-            GL.BindVertexArray(_vao);
-
-            // Set MVP matrices
-            var model = Matrix4.CreateScale(scale) * Matrix4.CreateTranslation(position);
-
-            // Note: Updated uniform names to match ShaderResource.cs ("model", "view", "projection")
-            GL.UniformMatrix4(GL.GetUniformLocation(_shaderProgram, "model"), false, ref model);
-            GL.UniformMatrix4(GL.GetUniformLocation(_shaderProgram, "view"), false, ref _view);
-            GL.UniformMatrix4(GL.GetUniformLocation(_shaderProgram, "projection"), false, ref _projection);
-
-            // Generate Cube Vertices
-            var data = GenerateCubeData(color);
-
-            // Upload and Draw
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.UseProgram(_shaderSolid); GL.BindVertexArray(_vaoSolid);
+            TK.Matrix4 model = TK.Matrix4.Identity;
+            UploadMatrices(_shaderSolid, ref model);
+            var r = color.r / 255f; var g = color.g / 255f; var b = color.b / 255f; var a = color.a / 255f;
+            float[] data = { v0.X, v0.Y, v0.Z, r, g, b, a, v1.X, v1.Y, v1.Z, r, g, b, a, v2.X, v2.Y, v2.Z, r, g, b, a };
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboSolid);
             GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, data.Length * sizeof(float), data);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 36); // 36 vertices in a cube
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
 
+        public void DrawTexturedTriangle(Vector3 v0, Vector2 uv0, Vector3 v1, Vector2 uv1, Vector3 v2, Vector2 uv2, int textureId)
+        {
+            EnsureInitialized();
+            GL.UseProgram(_shaderTex); GL.BindVertexArray(_vaoTex);
+            TK.Matrix4 model = TK.Matrix4.Identity;
+            UploadMatrices(_shaderTex, ref model);
+            GL.ActiveTexture(TextureUnit.Texture0); GL.BindTexture(TextureTarget.Texture2D, textureId);
+            float[] data = { v0.X, v0.Y, v0.Z, uv0.X, uv0.Y, 1, 1, 1, 1, v1.X, v1.Y, v1.Z, uv1.X, uv1.Y, 1, 1, 1, 1, v2.X, v2.Y, v2.Z, uv2.X, uv2.Y, 1, 1, 1, 1 };
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboTex);
+            GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, data.Length * sizeof(float), data);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+
+        public void DrawSprite(Vector3 position, float radius, int textureId)
+        {
+            EnsureInitialized();
+            GL.UseProgram(_shaderTex); GL.BindVertexArray(_vaoTex);
+            TK.Vector3 right = new(_view[0, 0], _view[1, 0], _view[2, 0]);
+            TK.Vector3 up = new(_view[0, 1], _view[1, 1], _view[2, 1]);
+            TK.Vector3 pos = new(position.X, position.Y, position.Z);
+
+            var v0 = pos - (right * radius) - (up * radius);
+            var v1 = pos + (right * radius) - (up * radius);
+            var v2 = pos + (right * radius) + (up * radius);
+            var v3 = pos - (right * radius) + (up * radius);
+
+            TK.Matrix4 model = TK.Matrix4.Identity;
+            UploadMatrices(_shaderTex, ref model);
+            GL.ActiveTexture(TextureUnit.Texture0); GL.BindTexture(TextureTarget.Texture2D, textureId);
+            float[] data = { v0.X, v0.Y, v0.Z, 0, 0, 1, 1, 1, 1, v1.X, v1.Y, v1.Z, 1, 0, 1, 1, 1, 1, v2.X, v2.Y, v2.Z, 1, 1, 1, 1, 1, 1, v0.X, v0.Y, v0.Z, 0, 0, 1, 1, 1, 1, v2.X, v2.Y, v2.Z, 1, 1, 1, 1, 1, 1, v3.X, v3.Y, v3.Z, 0, 1, 1, 1, 1, 1 };
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboTex);
+            GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, data.Length * sizeof(float), data);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+        }
+
+        public unsafe void Flush(RenderBatch batch)
+        {
+            EnsureInitialized();
+            if (batch.Solid3DVertices.Length > 0)
+            {
+                GL.UseProgram(_shaderSolid); GL.BindVertexArray(_vaoSolid);
+                TK.Matrix4 id = TK.Matrix4.Identity; UploadMatrices(_shaderSolid, ref id);
+                EnsureBufferCapacity(_vboSolid, ref _vboSolidCapacity, batch.Solid3DVertices.Length);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, _vboSolid);
+                GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, batch.Solid3DVertices.Length * sizeof(float), (IntPtr)batch.Solid3DVertices.Pointer);
+                GL.DrawArrays(PrimitiveType.Triangles, 0, batch.Solid3DVertices.Length / 7);
+            }
+            if (batch.Textured3DBatches.Count > 0)
+            {
+                GL.UseProgram(_shaderTex); GL.BindVertexArray(_vaoTex);
+                TK.Matrix4 id = TK.Matrix4.Identity; UploadMatrices(_shaderTex, ref id);
+                foreach (var kvp in batch.Textured3DBatches)
+                {
+                    if (kvp.Value.Count == 0) continue;
+                    GL.ActiveTexture(TextureUnit.Texture0); GL.BindTexture(TextureTarget.Texture2D, kvp.Key);
+                    var data = kvp.Value.ToArray();
+                    EnsureBufferCapacity(_vboTex, ref _vboTexCapacity, data.Length);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, _vboTex);
+                    GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, data.Length * sizeof(float), data);
+                    GL.DrawArrays(PrimitiveType.Triangles, 0, data.Length / 9);
+                }
+            }
             GL.BindVertexArray(0);
         }
 
-        // Helper to generate a 1x1x1 cube centered at the local origin
-        private float[] GenerateCubeData((int r, int g, int b, int a) c)
+        private void UploadMatrices(int shader, ref TK.Matrix4 model)
         {
-            var r = c.r / 255f;
-            var g = c.g / 255f;
-            var b = c.b / 255f;
-            var a = c.a / 255f;
-
-            // X, Y, Z, R, G, B, A (36 vertices for 12 triangles)
-            return new float[]
-            {
-                // Front face
-                -0.5f, -0.5f, 0.5f, r, g, b, a, 0.5f, -0.5f, 0.5f, r, g, b, a, 0.5f, 0.5f, 0.5f, r, g, b, a, 0.5f, 0.5f,
-                0.5f, r, g, b, a, -0.5f, 0.5f, 0.5f, r, g, b, a, -0.5f, -0.5f, 0.5f, r, g, b, a,
-                // Back face
-                -0.5f, -0.5f, -0.5f, r, g, b, a, -0.5f, 0.5f, -0.5f, r, g, b, a, 0.5f, 0.5f, -0.5f, r, g, b, a, 0.5f,
-                0.5f, -0.5f, r, g, b, a, 0.5f, -0.5f, -0.5f, r, g, b, a, -0.5f, -0.5f, -0.5f, r, g, b, a,
-                // Left face
-                -0.5f, 0.5f, 0.5f, r, g, b, a, -0.5f, 0.5f, -0.5f, r, g, b, a, -0.5f, -0.5f, -0.5f, r, g, b, a, -0.5f,
-                -0.5f, -0.5f, r, g, b, a, -0.5f, -0.5f, 0.5f, r, g, b, a, -0.5f, 0.5f, 0.5f, r, g, b, a,
-                // Right face
-                0.5f, 0.5f, 0.5f, r, g, b, a, 0.5f, -0.5f, -0.5f, r, g, b, a, 0.5f, 0.5f, -0.5f, r, g, b, a, 0.5f,
-                -0.5f, -0.5f, r, g, b, a, 0.5f, 0.5f, 0.5f, r, g, b, a, 0.5f, -0.5f, 0.5f, r, g, b, a,
-                // Top face
-                -0.5f, 0.5f, -0.5f, r, g, b, a, -0.5f, 0.5f, 0.5f, r, g, b, a, 0.5f, 0.5f, 0.5f, r, g, b, a, 0.5f, 0.5f,
-                0.5f, r, g, b, a, 0.5f, 0.5f, -0.5f, r, g, b, a, -0.5f, 0.5f, -0.5f, r, g, b, a,
-                // Bottom face
-                -0.5f, -0.5f, -0.5f, r, g, b, a, 0.5f, -0.5f, -0.5f, r, g, b, a, 0.5f, -0.5f, 0.5f, r, g, b, a, 0.5f,
-                -0.5f, 0.5f, r, g, b, a, -0.5f, -0.5f, 0.5f, r, g, b, a, -0.5f, -0.5f, -0.5f, r, g, b, a
-            };
+            GL.UniformMatrix4(GL.GetUniformLocation(shader, "model"), false, ref model);
+            GL.UniformMatrix4(GL.GetUniformLocation(shader, "view"), false, ref _view);
+            GL.UniformMatrix4(GL.GetUniformLocation(shader, "projection"), false, ref _projection);
         }
 
-        public void Dispose()
-        {
-            if (!_initialized) return;
-
-            GL.DeleteBuffer(_vbo);
-            GL.DeleteVertexArray(_vao);
-            // Note: We do NOT delete the _shaderProgram here anymore,
-            // because the GlResourceManager owns it and will clean it up!
-        }
+        private void EnsureBufferCapacity(int vbo, ref int cap, int req) { if (req <= cap) return; while (cap < req) cap *= 2; GL.BindBuffer(BufferTarget.ArrayBuffer, vbo); GL.BufferData(BufferTarget.ArrayBuffer, cap * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw); }
+        public void Dispose() { if (!_initialized) return; GL.DeleteBuffer(_vboSolid); GL.DeleteVertexArray(_vaoSolid); GL.DeleteBuffer(_vboTex); GL.DeleteVertexArray(_vaoTex); }
     }
 }
