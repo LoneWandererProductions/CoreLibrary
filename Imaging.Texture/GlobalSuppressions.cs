9e5a39764fac358958b8532e5566f229b9918591
LoneWandererProductions <peter.geinitz@freenet.de>
2026-07-21 13:12:41 +0200

    remove some unnecesary suppressions

--- Imaging.Texture/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Imaging.Texture.ColorConverter.HslToRgbTuple(System.Double,System.Double,System.Double)~System.ValueTuple{System.Byte,System.Byte,System.Byte}")]
-[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Imaging.Texture.TextureMathEngine.GenerateCanvas(System.Int32,System.Int32,System.Int32,System.Int32,System.Int32,System.Double,System.Double,System.Double,System.Int32,System.Int32,System.Byte,System.Byte,System.Byte,System.Byte,System.Byte,System.Byte,System.Byte)~Imaging.Texture.RawTextureBuffer")]
