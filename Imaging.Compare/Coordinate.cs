9065b9057a6be7670e8094a96ac216ade9303d3f
Wayfarer <petergeinitz@freenet.de>
2026-08-29 22:28:07 +0200

    cleanup compare a bit

--- Imaging.Compare/Coordinate.cs
+++ Imaging.Compare/Coordinate.cs
@@ -105,7 +105,7 @@ namespace Imaging.Compare
         /// <returns>
         ///     <c>true</c> if the specified <see cref="object" /> is equal to this instance; otherwise, <c>false</c>.
         /// </returns>
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             return obj is Coordinate other && Equals(other);
         }
